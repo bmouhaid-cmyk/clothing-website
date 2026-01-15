14eb910e66a3efec2ff5206e6c581885153b1737
Badr Mouhaid <badrmouhaid@MacBook-Air-de-Badr.local>
2026-01-15 19:13:04 +0100

    Fix: Enable next-intl with Verified Safe Configuration (Next 14 + v3)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -18,4 +20,4 @@ const nextConfig = {
     },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
