6d61ce54ac1a3d4ef6d904b51be9efcf782d3d91
Badr Mouhaid <badrmouhaid@MacBook-Air-de-Badr.local>
2026-01-15 18:37:57 +0100

    Fix: Inline routing config in middleware to avoid import issues

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { defineRouting } from 'next-intl/routing';
+
+const routing = defineRouting({
+    locales: ['fr', 'ar'],
+    defaultLocale: 'fr'
+});
 
 export default createMiddleware(routing);
 
